eb6363d7e33cad2c04f72248fd87fc519dfef252
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-02-22 23:56:12 +0900

    [Silver III] Title: 주유소, Time: 76 ms, Memory: 42684 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,13 +6,14 @@ distance_list = list(map(int, sys.stdin.readline().rstrip().split()))
 price_list = list(map(int, sys.stdin.readline().rstrip().split()))
 
 price = 0
-start = end = N - 2
-
-while start >= 0:
-  if start == 0 or price_list[start] < price_list[start - 1]:
-    price += price_list[start]*sum(distance_list[start:end + 1])
-    end = start - 1
+min_price_index = end_index = N - 2;
+# print("min index = %d"%price_list.index(min(price_list[:end_index + 1])))
+while end_index >= 0:
+  min_price = min(price_list[:end_index + 1])
+  min_price_index = price_list.index(min_price)          #리터당 주유비가 가장 저렴한 지점
   
-  start -= 1
-
+  price += price_list[min_price_index]*sum(distance_list[min_price_index:end_index + 1])
+  # print("price = %d, start = %d, end = %d"%(price, min_price_index, end_index))
+  end_index = min_price_index - 1 
+  
 print(price)
