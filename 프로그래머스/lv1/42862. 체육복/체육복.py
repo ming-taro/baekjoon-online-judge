c896e95bbf9095a4a88f50232a9c68be6f48a538
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-03-01 22:06:40 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -12,20 +12,22 @@ def solution(n, lost, reserve):
         student[index] += 1   #체육복 여분이 있는 학생의 체육복 +1
 
     for i in range(n):
-        if student[i] == 0:
-            if i == 0 and student[i + 1] == 2:
-                student[i + 1] -= 1
-                student[i] += 1
-            elif i == n - 1 and student[i - 1] == 2:
+        if student[i] != 0:
+            continue
+        
+        if i == 0 and student[1] == 2:  #0번은 1번에게만 빌릴 수 있음
+            student[1] -= 1
+            student[0] += 1
+        elif i == n - 1 and student[i - 1] == 2:  #마지막 번호는 전 번호 친구에게만 빌릴 수 있음
+            student[i - 1] -= 1
+            student[i] += 1
+        elif i > 0 and i < n - 1:
+            if student[i - 1] == 2:
                 student[i - 1] -= 1
                 student[i] += 1
-            elif i > 0 and i < n - 1:
-                if student[i - 1] == 2:
-                    student[i - 1] -= 1
-                    student[i] += 1
-                elif student[i + 1] == 2:
-                    student[i + 1] -= 1
-                    student[i] += 1
+            elif student[i + 1] == 2:
+                student[i + 1] -= 1
+                student[i] += 1
             
     for i in student:
         if i > 0:
