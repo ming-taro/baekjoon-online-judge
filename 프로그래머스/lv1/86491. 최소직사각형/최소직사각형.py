f009eef74accf60ebb654f5ec7bece6c14c51c39
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-03-01 23:37:29 +0900

    [level 1] Title: 최소직사각형, Time: 5.06 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,18 @@
+def solution(sizes):
+    answer = 0
+    
+    length = len(sizes)
+    
+    max_x = 0
+    max_y = 0
+    
+    for size_list in sizes:
+        x = max(size_list)
+        y = min(size_list)
+        
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
+        
+    answer = max_x*max_y 
+    
+    return answer
