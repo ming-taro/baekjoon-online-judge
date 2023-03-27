f307f13614413f1410aa7065c3e5e61fae7b5639
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-03-27 16:01:21 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -11,11 +11,9 @@ for _ in range(N):
 dp = [0 for _ in range(N + 1)]
 
 for i in reversed(range(N)):
-    if i + plan[i][0] > N:
+    if i + plan[i][0] > N:   #기간이 N을 넘어가는 경우 오늘 상담 포기, 다음날 상담으로 대체
         dp[i] = dp[i + 1]
-    elif i < N - 1:
+    else:                    #(오늘 상담) + (다음 상담부터 마지막날 상담) VS 오늘 상담을 포기한 경우(=다음날 상담~마지막날 상담)
         dp[i] = max(plan[i][1] + dp[i + plan[i][0]], dp[i + 1]) 
-    else:
-        dp[i] = plan[i][1]
 
 print(dp[0])
