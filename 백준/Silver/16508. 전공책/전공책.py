ed4e9fe2a0c1071b808cb16cb36a508a0bb489bb
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-04-04 15:53:02 +0900

    [Silver III] Title: 전공책, Time: 1116 ms, Memory: 35348 KB -BaekjoonHub

---
+++ 백준/Silver/16508. 전공책/전공책.py
@@ -0,0 +1,58 @@
+import sys
+from collections import Counter
+from itertools import combinations
+
+word = Counter(list(sys.stdin.readline().rstrip()))
+
+N = int(sys.stdin.readline().rstrip())
+book_list = []
+
+for _ in range(N):
+    price, title = map(str, sys.stdin.readline().rstrip().split())
+    book_list.append([title, int(price)])
+
+number_list = [i for i in range(N)]
+
+
+def combine_title_list(index_list):
+    title_list = []
+
+    for index in index_list:
+        title_list += book_list[index][0]
+
+    return title_list
+
+
+def is_word_cominations_possible(word, title_list):
+    word_counter = Counter(word)
+    title_counter = Counter(title_list)
+
+    for word in word_counter:
+        if word_counter[word] > title_counter[word]:
+            return False
+
+    return True
+
+
+def get_total_price(index_list):
+    price = 0
+
+    for index in index_list:
+        price += book_list[index][1]
+
+    return price
+
+
+min_price = 1600001
+
+for n in range(1, N + 1):
+    index_list = list(combinations(number_list, n))
+
+    for index in index_list:
+        if is_word_cominations_possible(word, combine_title_list(index)):
+            min_price = min(min_price, get_total_price(index))
+
+if min_price == 1600001:
+    print(-1)
+else:
+    print(min_price)
