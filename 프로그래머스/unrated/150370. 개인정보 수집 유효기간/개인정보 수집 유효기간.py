9a56874e14bef6e0133e6dff577051a981669100
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-05-09 14:40:25 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.81 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,58 @@
+def is_within_range(today, period, term):
+    today_year, today_month, today_day = map(int, today.split('.'))
+    
+    year, month, day = map(int, term.split('.'))
+    month += period
+    day -= 1
+    
+    print("today: ", today_year, today_month, today_day)
+    print("after: ", year, month, day)
+    print("period: ", period)
+    print()
+    
+    if day <= 0:
+        day += 28
+        month -= 1
+    
+    if month <= 0:
+        month += 12
+        year -= 1
+    
+    while day > 28:
+        month += 1
+        day -= 28
+    
+    while month > 12:
+        year += 1
+        month -= 12
+    
+    print("today: ", today_year, today_month, today_day)
+    print("after: ", year, month, day)
+    
+    if (today_year*10000 + today_month*100 + today_day > 
+        year*10000 + month*100 + day):
+        print("======True======")
+        return True
+    print("======False======")
+    return False
+    
+
+def solution(today, terms, privacies):
+    answer = []
+    privacy_dic = {}
+    
+    for term in terms:
+        privacy, period = term.split()
+        privacy_dic[privacy] = int(period)
+    
+    number = 1
+    for item in privacies:
+        term, privacy = item.split()
+        period = privacy_dic[privacy]
+        
+        if is_within_range(today, period, term):
+            answer.append(number)
+        number += 1
+            
+        
+    return answer
