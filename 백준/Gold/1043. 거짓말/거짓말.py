ffc971d20cbf66d9167544cbfda09598a30f105c
ming-taro <100523178+ming-taro@users.noreply.github.com>
2023-08-19 14:56:39 +0900

    [Gold IV] Title: 거짓말, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -2,30 +2,28 @@ import sys
 from collections import deque
 
 N, M = map(int, sys.stdin.readline().rstrip().split(" "))
-
 person = sys.stdin.readline().rstrip()
-is_possible = True
 
-if person == '0':
-  is_possible = False
+if person == '0':      #진실을 말해야하는 사람이 0명인 경우 -> 모든 파티 참석 가능
   for _ in range(M):
-    person_list = sys.stdin.readline().rstrip()
+    party_attendees = sys.stdin.readline().rstrip()
   print(M)
   exit(0)
 
 true_person = list(map(int, person.split(" ")))[1:]   #진실을 말해야하는 사람
-person_list = []
 
-result = 0
+party_attendees = []
+total_party = 0
 
-flag_list = [[] for _ in range(N + 1)]
+attendees_list = [[] for _ in range(N + 1)]
 
 for _ in range(M):
-  person_list.append(list(map(int, sys.stdin.readline().rstrip().split(" ")))[1:])
-  for index in range(len(person_list[-1])):
-    person = person_list[-1]
-    flag_list[person[index]] += person[:index]
-    flag_list[person[index]] += person[index + 1:]
+  attendees = list(map(int, sys.stdin.readline().rstrip().split(" ")))[1:]
+  party_attendees.append(attendees)
+  
+  for index in range(len(attendees)):  #attendees[index]와 같은 파티에 참석하는 사람들 명단 저장
+    attendees_list[attendees[index]] += attendees[:index]
+    attendees_list[attendees[index]] += attendees[index + 1:]
 
 queue = deque(true_person)
 visited = [False for _ in range(N + 1)]
@@ -33,21 +31,24 @@ visited = [False for _ in range(N + 1)]
 for p in true_person:
   visited[p] = True
 
+#while문을 순회하며 진실을 말해야하는 사람과 같은 파티에 참석한 사람들을 체크함
 while queue:
   node = queue.popleft()
   
-  for v in flag_list[node]:
+  for v in attendees_list[node]:
     if visited[v] == False:
       visited[v] = True
       queue.append(v)
 
-for person in person_list:
-  flag = True
-  for p in person:
-    if visited[p]:
-      flag = False
+for attendee in party_attendees:
+  is_possible_to_attend = True
+  
+  for v in attendee:
+    if visited[v]:  #진실을 말해야하는 사람이 있는 경우, 파티에 참가할 수 X
+      is_possible_to_attend = False
       break
-  if flag:
-    result += 1
+    
+  if is_possible_to_attend:
+    total_party += 1
 
-print(result)
+print(total_party)
