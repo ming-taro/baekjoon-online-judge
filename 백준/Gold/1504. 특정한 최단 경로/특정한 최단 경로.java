6a2e09da1f971e2f9d7d8ed669983ea97ad22eb8
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-01-16 22:19:27 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 704 ms, Memory: 69688 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,91 +1,102 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.*;
-
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
-
-    public Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weight = o.weight;
-    }
-}
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static ArrayList<ArrayList<Node>> nodes = new ArrayList<>();
-    private static int N;
-    private static final int INF = 200_000_000; //200_000*1_000
+    private static int N, E, Ans; // N:정점, E:간선
+    private static int INF = 200000000; //200,000 * 1,000
+    private static ArrayList<Node>[] adjList;
+    private static int[] cost;
+    private static boolean[] visited;
+    private static class Node implements Comparable<Node>{
+        int dest, cost; //dest: 목적지, cost: 거리
+        public Node(int dest, int cost){
+            this.dest = dest;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Node o){
+            return this.cost - o.cost; //cost 기준 오름차순 정렬
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Ans = 0;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-        String[] input = reader.readLine().split(" ");
-        N = Integer.parseInt(input[0]);
-        int E = Integer.parseInt(input[1]);
+        adjList = new ArrayList[N+1];
+        cost = new int[N+1];
+        visited = new boolean[N+1];
 
-        for (int i = 0; i <= N; i++) { //연결노드
-            nodes.add(new ArrayList<>());
+        //인접리스트 초기화
+        for (int i = 0; i < N+1; i++) {
+            adjList[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < E; i++) {
-            int[] nodeInput = Arrays.stream(reader.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-
-            int start = nodeInput[0];
-            int end = nodeInput[1];
-            int weight = nodeInput[2];
-
-            nodes.get(start).add(new Node(end, weight));  //start->end, end->start
-            nodes.get(end).add(new Node(start, weight));
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()); //출발지
+            int b = Integer.parseInt(st.nextToken()); //도착지
+            int c = Integer.parseInt(st.nextToken()); //비용(거리)
+            adjList[a].add(new Node(b, c));//단방향인 경우 이것만 해도 됨
+            adjList[b].add(new Node(a, c));//무방향(양방향)은 이것도 해야 함
         }
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
 
-        input = reader.readLine().split(" ");
-        int start = Integer.parseInt(input[0]);
-        int end = Integer.parseInt(input[1]);
+        int ans1 = 0; // 1 > v1 > v2 > N
+        ans1 += dijkstra(1, v1);
+        ans1 += dijkstra(v1, v2);
+        ans1 += dijkstra(v2, N);
 
-        int firstPath = dijkstra(1, start) + dijkstra(start, end) + dijkstra(end, N);
-        int secondPath = dijkstra(1, end) + dijkstra(end, start) + dijkstra(start, N);
+        int ans2 = 0; // 1 > v2 > v1 > N
+        ans2 += dijkstra(1, v2);
+        ans2 += dijkstra(v2, v1);
+        ans2 += dijkstra(v1, N);
 
-        int result = (firstPath >= INF && secondPath >= INF) ? -1 : Math.min(firstPath, secondPath);
-        System.out.println(result);
+        if(ans1 >= INF && ans2 >= INF) Ans = -1; //경로가 없는 경우
+        else Ans = Math.min(ans1, ans2); //경로가 있을 경우 더 작은 값
+
+        bw.write(Ans+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
+    private static int dijkstra(int start, int end){
+        //최저비용 기준 탐색경로 저장 위한 우선순위 큐
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Arrays.fill(visited, false); //방문여부 체크 배열 false로 초기화
+        Arrays.fill(cost, INF); //cost 배열 INF로 초기화
 
-    private static int dijkstra(int start, int end) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        int[] distance = new int[N + 1];     //정점거리
-//        boolean[] visited = new boolean[N + 1];  //방문확인
+        cost[start] = 0; //시작점이기 때문에 거리 0으로 셋팅
+        pq.add(new Node(start, 0));
 
-        Arrays.fill(distance, INF);
-        queue.offer(new Node(start, 0));
-        distance[start] = 0; //start->start 거리 = 0
+        while (!pq.isEmpty()){
+            Node now = pq.poll();
 
-        while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            int current = currentNode.end;       //현재 위치한 정점
+            if(visited[now.dest]) continue; //방문했던 정점이면 스킵
+            visited[now.dest] = true; //미방문이면 방문으로(true)로 셋팅하고 for문 처리
 
-//            if (visited[current]) {  //다익스트라 알고리즘 -> 가장 작은 정점부터 방문하기 때문에 방문 처리를 해줌
-//                continue;
-//            }
-
-//            visited[current] = true; //우선순위 큐에서 꺼냈을 때 visited표시 -> 다익스트라를 통해 start에서 현재 정점까지의 가중치가 가장 작음을 보장하기 때문
-
-            for (Node node: nodes.get(current)) {  //node.end -> 현재 정점과 연결된 정점 번호
-                int newPath = distance[current] + node.weight;           //start->현재 노드까지의 거리 + 현재 노드->연결된 노드까지의 거리 == start->연결된 노드까지의 거리
-                if (newPath < distance[node.end]) {//start->연결된 노드까지의 기존 길 보다 현재 정점을 거쳐서 가는 길의 가중치가 더 적은 경우
-                    distance[node.end] = newPath;
-                    queue.offer(new Node(node.end, distance[node.end])); //거리 가중치를 누적한 결과값을 weight에 저장
+            //간선 탐색
+            for (Node next:adjList[now.dest]) {
+                if(cost[next.dest] > next.cost + now.cost){
+                    cost[next.dest] = next.cost + now.cost;
+                    pq.add(new Node(next.dest, cost[next.dest]));
                 }
             }
         }
 
-        return distance[end];  //end까지의 최종 거리 가중치(priority queue를 사용했기 때문에 최소값이 저장됨)
+        return cost[end]; //목적지 end 까지의 최단거리
     }
 }
