d9ad79f0e022b229ed802754aecc6f755db5cc7f
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-02-13 21:59:56 +0900

    [Gold III] Title: 파티, Time: 224 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -20,7 +20,6 @@ class Node implements Comparable<Node> {
 
 public class Main {
     private static int N;
-    private static List<Node>[] paths;
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -31,10 +30,15 @@ public class Main {
         int M = Integer.parseInt(stringTokenizer.nextToken());
         int X = Integer.parseInt(stringTokenizer.nextToken());
 
-        paths = new ArrayList[N + 1];
+        List<Node>[] paths = new ArrayList[N + 1];
+        List<Node>[] reversePaths = new ArrayList[N + 1]; //X->각 정점 들로 가는 최단 거리
         for (int i = 1; i <= N; i++) {
             paths[i] = new ArrayList<>();
+            reversePaths[i] = new ArrayList<>();
         }
+
+        int[] distance = new int[N + 1];
+        int[] reverseDistance = new int[N + 1];
 
         for (int i = 0; i < M; i++) {
             stringTokenizer = new StringTokenizer(reader.readLine());
@@ -43,24 +47,27 @@ public class Main {
             int end = Integer.parseInt(stringTokenizer.nextToken());
             int time = Integer.parseInt(stringTokenizer.nextToken());
 
-            paths[start].add(new Node(end, time)); //단방향 도로
+            paths[start].add(new Node(end, time)); //X에서 정점으로 가는 길
+            reversePaths[end].add(new Node(start, time)); //도로를 반대 방향으로 저장한 후 X를 출발 지점으로 하는 알고리즘을 수행하면 X를 출발점으로 각 정점까지의 최단거리 == 각 정점에서 X까지의 최단거리
         }
 
-        int maxTime = 0;
+        dijkstra(X, paths, distance);                //X->정점
+        dijkstra(X, reversePaths, reverseDistance);  //정점->X
 
+        int maxTime = 0;
+        
         for (int i = 1; i <= N; i++) {
-            maxTime = Math.max(maxTime, dijkstra(i, X) + dijkstra(X, i));
+            maxTime = Math.max(maxTime, distance[i] + reverseDistance[i]);
         }
 
         System.out.println(maxTime);
     }
 
-    private static int dijkstra(int start, int end) {
+    private static void dijkstra(int start, List<Node>[] paths, int[] distance) {
         Queue<Node> queue = new PriorityQueue<>();
         queue.add(new Node(start, 0));
 
         boolean[] visited = new boolean[N + 1];
-        int[] distance = new int[N + 1];
 
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[start] = 0;
@@ -81,7 +88,5 @@ public class Main {
                 }
             }
         }
-
-        return distance[end];
     }
 }
