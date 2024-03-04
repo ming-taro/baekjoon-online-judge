748401e213d3a0271a7c5233f968bf38ba0e7cde
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-03-04 23:51:02 +0900

    [Gold IV] Title: 우체국, Time: 564 ms, Memory: 47004 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,51 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static class Home implements Comparable<Home>{
-        long number;
-        long people;
+class Node implements Comparable<Node>{
+    long number;
+    long people;
 
-        public Home(long number, long people) {
-            this.number = number;
-            this.people = people;
-        }
-
-        @Override
-        public int compareTo(Home o) {
-            return (int) (number - o.number);
-        }
+    public Node(long number, long people) {
+        this.number = number;
+        this.people = people;
     }
+
+    @Override
+    public int compareTo(Node node) {
+        return (int) (this.number - node.number);
+    }
+}
+
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
 
         int N = Integer.parseInt(reader.readLine());
+        List<Node> village = new ArrayList<>();
 
-        ArrayList<Home> distance = new ArrayList<>();
-        long totalPeople = 0;
+        long total = 0;
 
         for (int i = 0; i < N; i++) {
-            stringTokenizer = new StringTokenizer(reader.readLine());
-            long X = Integer.parseInt(stringTokenizer.nextToken());
-            long A = Integer.parseInt(stringTokenizer.nextToken());
-
-            distance.add(new Home(X, A));
-            totalPeople += A;
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            village.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            total += village.get(i).people;
         }
 
-        Collections.sort(distance);
-        long sum = 0;
+        Collections.sort(village);
 
-        for (Home home: distance) {
-            sum += home.people;
-            if (sum >= (double) totalPeople / 2.0) {
-                System.out.println(home.number);
-                return;
+        long left = 0;
+        long right = total;
+
+        long minDifference = total;
+        long number = village.get(0).number;
+
+        for (Node current: village) {  //왼쪽 오른쪽 사람 수의 균형이 알맞은 위치가 우체국의 위치
+            right -= current.people;
+            if (Math.abs(right - left) < minDifference) {
+                number = current.number;
+                minDifference = Math.abs(right - left);
             }
+            if (left > right) {
+                break;
+            }
+            left += current.people;
         }
+
+        System.out.println(number);
     }
 }
