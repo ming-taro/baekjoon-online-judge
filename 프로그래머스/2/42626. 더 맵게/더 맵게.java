beeda169d5e825f48872552bb462677e52a0be72
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-07-02 09:49:56 +0900

    [level 2] Title: 더 맵게, Time: 1682.71 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        Queue<Integer> queue = new PriorityQueue<>();
+        
+        for (int scov: scoville) {
+            queue.offer(scov);
+        }
+        // System.out.println(queue);
+        
+        while (queue.size() > 1) {
+            int first = queue.poll();
+            if (first >= K) {
+                break;
+            }
+            int second = queue.poll();
+            
+            queue.offer(first + second * 2);
+            answer++;
+            // System.out.println(queue);
+        }
+        
+        if (queue.peek() >= K) {
+            return answer;
+        }
+        
+        return -1;
+    }
+}
+/*
+->모든 음식의 스코빌 지수를 K이상으로 만들고 싶다
+->스코빌 지수가 가장 낮은 두 개 음식 = 첫번째 + 두번째*2
+
+*/
