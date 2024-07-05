5f7274b091440976f9c21ab6d53ac64e36f2d8f5
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-07-05 16:44:19 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.21 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.24 ms
+메모리: 75.1 MB, 시간: 1.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 23:17:50
+2024년 07월 05일 16:44:16
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        Deque<Character> queue = new ArrayDeque<>();
+        
+        answer = new_id.toLowerCase(); // 1단계
+        answer = answer.replaceAll("[^.a-z0-9_-]", ""); // 2단계
+        answer = answer.replaceAll("[\\.]+", "."); // 3단계
+        
+        if (answer.length() > 0 && answer.charAt(0) == '.') { // 4단계
+            answer = answer.substring(1);
+        }
+        if (answer.length() > 0 && answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+
+        if (answer.isEmpty()) { // 5단계
+            answer = "a";
+        }
+        
+        if (answer.length() >= 16) { // 6단계
+            answer = answer.substring(0, 15);
+        }
+         if (answer.length() > 0 && answer.charAt(answer.length() - 1) == '.') {
+            answer = answer.substring(0, answer.length() - 1);
+        }
+        
+        if (answer.length() <= 2) {
+            char target = answer.charAt(answer.length() - 1);
+            int repeat = 3 - answer.length();
+            
+            for (int i = 0; i < repeat; i++) {
+                answer += target;
+            }
+        }
+        
+        return answer;
+    }
+}
+/*
+아이디: 소문자, 숫자, '-', '_', '.'
+       ->'.'는 끝에 사용할 수 없다, 연속으로 사용할 수 없다
+*/
