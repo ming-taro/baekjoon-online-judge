c96c10141aa8f1dbd9aa0a50f44c14542d8efdd4
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-07-16 14:08:22 +0900

    [level 2] Title: 소수 찾기, Time: 10.67 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,32 @@
+import math
+from itertools import permutations
+
+def is_prime(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+    
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number%i == 0:
+            return False
+    
+    return True
+
+def solution(numbers):
+    answer = 0
+    
+    numbers = [number for number in numbers]
+    num_list = set()
+    
+    for i in range(1, len(numbers) + 1):
+        perm_list = list(permutations(numbers, i))
+        for num in perm_list:
+            num_list.add(int("".join(num)))
+                         
+    for num in num_list:
+        print(num)
+        if is_prime(num):
+            answer += 1
+        
+    return answer
