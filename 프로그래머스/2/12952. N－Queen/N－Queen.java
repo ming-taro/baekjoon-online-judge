e5646a109e52adb93f3cc3d0a1ca59c7e4850c99
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-08-08 12:57:44 +0900

    [level 2] Title: N-Queen, Time: 282.11 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,89 +1,71 @@
 import java.util.*;
 
-class Solution {
-    static class Node {
-        int row;
-        int col;
-        
-        public Node(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
+class Node {
+    int row;
+    int col;
     
-    private static int N;
-    private static List<Node>[] nodes;
-    private static boolean[][] board;
-    private static boolean[] columnList;
-    private static int result = 0;
+    public Node(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class Solution {
+    private static int[][] board;
+    private static int n;
+    private static int answer = 0;
     
     public int solution(int n) {
-        this.N = n;
-        nodes = new ArrayList[N];
-        board = new boolean[N][N];
-        columnList = new boolean[N];
+        this.n = n;
+        board = new int[n][n];
         
-        checkQueen(0);
+        dfs(0);
         
-        return result;
+        return answer;
     }
     
-    private static void checkQueen(int row) {
-        if (row == N) {
-            result++;
+    private static void dfs(int row) {
+        if (row == n) {
+            answer++;
             return;
         }
         
-        for (int col = 0; col < N; col++) {
-            Node node = new Node(row, col);
-            
-            if (!columnList[col] && isDiagonalPossible(node)) { //퀸 -> 같은행, 같은열, 대각선
-                board[node.row][node.col] = true;
-                columnList[node.col] = true;
-                checkQueen(row + 1);
-                board[node.row][node.col] = false;
-                columnList[node.col] = false;
+        for (int col = 0; col < n; col++) {
+            if (check(new Node(row, col))) {
+                board[row][col] = 1;
+                dfs(row + 1);
+                board[row][col] = 0;
             }
         }
     }
     
-    private static boolean isDiagonalPossible(Node node) {
-        //위왼쪽 대각선
-        for (int row = node.row - 1, col = node.col - 1; row >= 0 && col >= 0; row--, col--) {  
-            if (board[row][col]) {
+    private static boolean check(Node node) {
+        for (int r = 0; r < n; r++) {      // 열 검사
+            if (board[r][node.col] == 1) {
                 return false;
             }
         }
         
-        //위오른쪽 대각선
-        for (int row = node.row - 1, col = node.col + 1; row >= 0 && col < N; row--, col++) {  
-            if (board[row][col]) {
+        for (int i = 1; i < n; i++) {
+            int topRow = node.row - i;
+            int downRow = node.row + i;
+            int leftCol = node.col - i;
+            int rightCol = node.col + i;
+            
+            if (isValid(topRow, leftCol) && board[topRow][leftCol] == 1
+                || isValid(topRow, rightCol) && board[topRow][rightCol] == 1) {  // 좌우측 상단 대각선
                 return false;
             }
-        }
-        
-        //아래왼쪽 대각선
-        for (int row = node.row + 1, col = node.col - 1; row < N && col >= 0; row++, col--) {  
-            if (board[row][col]) {
-                return false;
-            }
-        }
-        
-        //아래오른쪽 대각선
-        for (int row = node.row + 1, col = node.col + 1; row < N && col < N; row++, col++) {  
-            if (board[row][col]) {
+            if (isValid(downRow, leftCol) && board[downRow][leftCol] == 1
+                || isValid(downRow, rightCol) && board[downRow][rightCol] == 1) {// 좌우측 하단 대각선
                 return false;
             }
         }
         
         return true;
     }
+    
+    private static boolean isValid(int row, int col) {
+        return row >= 0 && row < n && col >= 0 && col < n;
+    }
 }
-
-/*
-(0,0) (0, 1) (0, 2) (0, 3)
-(1,0) (1, 1) (1, 2) (1, 3)
-(2,0) (2, 1) (2, 2) (2, 3)
-(3,0) (3, 1) (3, 2) (3, 3)
-
-*/
