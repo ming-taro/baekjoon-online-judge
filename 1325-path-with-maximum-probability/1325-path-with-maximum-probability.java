369c74a0f1720510a9c9854c7b16cce983a9cf14
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-08-29 16:19:58 +0900

    Time: 271 ms (5.06%), Space: 57.4 MB (41.77%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -13,22 +13,22 @@ class Solution {
         for (int i = 0; i < n; i++) {
             nodes[i] = new ArrayList<>();
         }
-
         for (int i = 0; i < edges.length; i++) {
             nodes[edges[i][0]].add(new Node(edges[i][1], succProb[i]));
             nodes[edges[i][1]].add(new Node(edges[i][0], succProb[i]));
         }
 
         double[] distance = new double[n];
-        distance[start_node] = 1.0;
+        distance[start_node] = 1;
 
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(start_node);
 
         while (!queue.isEmpty()) {
             int current = queue.poll();
+            System.out.println("current: " + current);
             for (Node next: nodes[current]) {
-                double value = distance[current] * next.value;
+                double value = next.value * distance[current];
                 if (value > distance[next.num]) {
                     distance[next.num] = value;
                     queue.offer(next.num);
