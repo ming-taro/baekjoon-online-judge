8ca5fe67bedd4f53546e7d226e7cf40557017a02
ming-taro <100523178+ming-taro@users.noreply.github.com>
2024-08-30 11:11:08 +0900

    Time: 10 ms (85.58%), Space: 48.7 MB (38.54%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,14 +1,15 @@
 class Node {
     int num;
-    int time;
-    public Node(int num, int time) {
+    int value;
+    public Node(int num, int value) {
         this.num = num;
-        this.time = time;
+        this.value = value;
     }
 }
 
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        int answer = 0;
         List<Node>[] nodes = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             nodes[i] = new ArrayList<>();
@@ -17,32 +18,30 @@ class Solution {
             nodes[time[0]].add(new Node(time[1], time[2]));
         }
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(k);
-
         int[] distance = new int[n + 1];
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[k] = 0;
+        
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(new Node(k, 0));
 
         while (!queue.isEmpty()) {
-            int current = queue.poll();
-            for (Node next: nodes[current]) {
-                int value = distance[current] + next.time;
+            Node current = queue.poll();
+            for (Node next: nodes[current.num]) {
+                int value = distance[current.num] + next.value;
                 if (value < distance[next.num]) {
                     distance[next.num] = value;
-                    queue.offer(next.num);
+                    queue.offer(next);
                 }
             }
-        }        
+        }
 
-        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (distance[i] == Integer.MAX_VALUE) {
                 return -1;
             }
             answer = Math.max(answer, distance[i]);
         }
-
         return answer;
     }
 }
