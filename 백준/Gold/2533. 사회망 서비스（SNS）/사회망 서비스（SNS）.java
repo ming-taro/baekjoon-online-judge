ce78dd4c06bc27a5cccc5ba37880693b790bed1d
ming-taro <100523178+ming-taro@users.noreply.github.com>
2025-01-06 17:44:16 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1936 ms, Memory: 404004 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -1,62 +1,52 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
- 
+
 public class Main {
-    final static int TRUE = 1;
-    final static int FALSE = 0;
-    static boolean[] visited;
-    static List<Integer> graph[];
-    static int[][] dp;
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-        int N = Integer.parseInt(br.readLine());
- 
-        graph = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        dp = new int[N+1][2];
- 
-        for(int i=0;i<=N;i++){
-            graph[i] = new ArrayList<>();
+    private static List<Integer>[] nodes;
+    private static boolean[] visited;
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        visited = new boolean[N + 1];
+        nodes = new ArrayList[N + 1];
+        for (int i = 1; i <=N; i++) {
+            nodes[i] = new ArrayList<>();
         }
- 
-        for(int i=0;i<N-1;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
- 
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
- 
-            graph[from].add(to);
-            graph[to].add(from);
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            nodes[u].add(v);
+            nodes[v].add(u);
         }
- 
-        findCount(1,TRUE);
- 
-        System.out.println(Math.min(dp[1][TRUE],dp[1][FALSE]));
+
+        visited[1] = true;
+        dfs(1);
+        System.out.println(answer);
     }
- 
-    public static int findCount(int curr,int valid){
-        if(visited[curr]){
-            return dp[curr][valid];
-        }
-        visited[curr] = true;
- 
-        dp[curr][TRUE] = 1;
-        dp[curr][FALSE] = 0;
- 
-        for(int next : graph[curr]){
-            if(visited[next]){
-                continue;
+
+    private static int dfs(int current) {
+        boolean flag = false;
+
+        for (int next: nodes[current]) {
+            if (visited[next]) continue;
+            visited[next] = true;
+            if (dfs(next) == 0) {
+                flag = true;
             }
-            int nextTrue = findCount(next, TRUE);
-            int nextFalse = findCount(next, FALSE);
- 
-            dp[curr][FALSE] += nextTrue;
-            dp[curr][TRUE] += Math.min(nextTrue,nextFalse);
         }
- 
-        return dp[curr][valid];
+
+        if (flag) {
+            answer++;  // 자식중 1명이라도 얼리가 아니라면 => 부모는 무조건 얼리
+            return 1;
+        }
+        return 0; // 자식들이 모두 얼리 or leaf라면 => 부모는 얼리일 필요X
     }
 }
