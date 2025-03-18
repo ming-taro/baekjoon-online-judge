e174c1ff1a27a88f6e1dbb5c532380fe27f1709e
ming-taro <hera002008@naver.com>
2025-03-18 20:56:56 +0900

    [Gold IV] Title: 타임머신, Time: 276 ms, Memory: 27120 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,93 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.management.loading.MLet;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    private static int INF = 5_000_000;
+class Node {
+    int val;
+    int weight;
+    public Node(int val, int weight) {
+        this.val = val;
+        this.weight = weight;
+    }
+}
+
+class Main {
     private static int N;
     private static ArrayList<Node>[] nodes;
-    private static long[] distance;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
 
-        stringTokenizer = new StringTokenizer(reader.readLine());
-        N = Integer.parseInt(stringTokenizer.nextToken());      //버스
-        int M = Integer.parseInt(stringTokenizer.nextToken());  //노선 수
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        nodes = new ArrayList[N + 1];  //간선
-        distance = new long[N + 1];     //start->정점 최단 거리
-
+        nodes = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             nodes[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
-            stringTokenizer = new StringTokenizer(reader.readLine());
-            int A = Integer.parseInt(stringTokenizer.nextToken());
-            int B = Integer.parseInt(stringTokenizer.nextToken());
-            int C = Integer.parseInt(stringTokenizer.nextToken());
-
+            st = new StringTokenizer(reader.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
             nodes[A].add(new Node(B, C));
         }
 
-        if(bellmanFord(1, N)) {     //음수 사이클이 있는 경우
+        long[] distance = new long[N + 1];
+
+        if (isNegativeCycle(distance, 1)) {
             System.out.println(-1);
             return;
         }
 
-        StringJoiner result = new StringJoiner("\n");
-
         for (int i = 2; i <= N; i++) {
-            if (distance[i] == INF) {
-                result.add("-1");  //경로X
-                continue;
-            }
-            result.add(distance[i] + "");
+            System.out.println(distance[i] == Integer.MAX_VALUE ? -1 : distance[i]);
         }
-
-        System.out.println(result);
     }
 
-    private static boolean bellmanFord(int start, int end) {
-        boolean isUpdated = false;
+    private static boolean isNegativeCycle(long[] distance, int start) {
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
 
-        Arrays.fill(distance, INF);
-        distance[start] = 0;  //자기자신은 0으로 초기화
-
-        //(N - 1)회 동안 최단거리 초기화
-        for (int i = 1; i < N; i++) {
-            isUpdated = false;
-            for (int j = 1; j <= N; j++) {
-                if (distance[j] == INF) {  //연결된 길이 없음 -> 첫 시작은 distance[start]=0인 start와 연결된 노드부터 거리 계산을 시작함
-                    continue;
+        for (int i = 0; i < N - 1; i++) {   // (n - 1)회 시행
+            for (int j = 1; j <= N; j++) {  // 거쳐 가는 노드(start - > j -> j.next == distance[j.next]) vs distance[end](== start -> end)
+                if (distance[j] == Integer.MAX_VALUE) continue; // 길 없음
+                for (Node next: nodes[j]) {
+                    distance[next.val] = Math.min(distance[next.val], distance[j] + next.weight);
                 }
-
-                for (Node node: nodes[j]) { //현재 정점과 연결된, 즉 start에서 end정점들 까지의 거리를 갱신하려함
-                    long newNode = distance[j] + node.weight;  //(정점 -> 현재) + (현재 -> 다음 노드)
-                    if (newNode < distance[node.end]) {
-                        distance[node.end] = newNode;
-                        isUpdated = true;
-                    }
-                }
-            }
-
-            //더 이상 갱신할 최단 거리가 없는 경우 탐색을 종료함
-            if (!isUpdated) {
-                break;
             }
         }
 
-        //N번째에서 한번 더 업데이트가 발생 = 음수 사이클 존재
-        for (int i = 1; i <= N; i++) {
-            if (distance[i] == INF) {
-                continue;
-            }
-
-            for (Node node: nodes[i]) {
-                if (distance[i] + node.weight < distance[node.end]) {
+        for (int j = 1; j <= N; j++) {
+            if (distance[j] == Integer.MAX_VALUE) continue;
+            for (Node next: nodes[j]) {
+                if (distance[j] + next.weight < distance[next.val]) {
                     return true;
                 }
             }
@@ -95,19 +71,8 @@ public class Main {
 
         return false;
     }
-
-    static class Node {
-        int end;
-        int weight;
-
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
-        }
-    }
 }
 /*
-A->B 버스 노선 = C 시간
-->단방향
-->C가 음수인 경우도 있음 = 밸만 포드 알고리즘
-*/
+벨만-포드 : 노드 - 노드간 최단 거리 (가중치 음수 가능)
+->매번 모든 간선 확인
+ */
