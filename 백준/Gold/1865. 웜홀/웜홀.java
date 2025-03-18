f9e2c79c0ac486ee94bc9cb290ad492409f7beab
ming-taro <hera002008@naver.com>
2025-03-19 03:36:38 +0900

    [Gold III] Title: 웜홀, Time: 744 ms, Memory: 38480 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,127 +1,87 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.management.loading.MLet;
+import java.io.*;
 import java.util.*;
 
 class Node {
-    int end;
-    int weight;
-
-    public Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
+    int val;
+    int time;
+    public Node(int val, int time) {
+        this.val = val;
+        this.time = time;
     }
 }
 
-public class Main {
-    private static int INF = 5_000_000;
-    private static int N;
-    private static int[] distance;
-    private static ArrayList<Node>[] nodes;
-
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
 
         int TC = Integer.parseInt(reader.readLine());
-        StringBuilder result = new StringBuilder();
+        for (int tc = 0; tc < TC; tc++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < TC; i++) {
-            stringTokenizer = new StringTokenizer(reader.readLine());
-            N = Integer.parseInt(stringTokenizer.nextToken());     //지점
-            int M = Integer.parseInt(stringTokenizer.nextToken()); //도로
-            int W = Integer.parseInt(stringTokenizer.nextToken()); //웜홀
-
-            distance = new int[N + 1];
-            nodes = new ArrayList[N + 1];
-            for (int j = 1; j <= N; j++) {
-                nodes[j] = new ArrayList<>();
+            ArrayList<Node>[] nodes = new ArrayList[N + 1];
+            for (int i = 1; i <= N; i++) {
+                nodes[i] = new ArrayList<>();
             }
 
-            for (int j = 0; j < M; j++) { //도로 정보(양방향)
-                stringTokenizer = new StringTokenizer(reader.readLine());
-                int S = Integer.parseInt(stringTokenizer.nextToken());
-                int E = Integer.parseInt(stringTokenizer.nextToken());
-                int T = Integer.parseInt(stringTokenizer.nextToken()); //소요 시간
-
+            for (int i = 0; i < M; i++) {  // 도로
+                st = new StringTokenizer(reader.readLine());
+                int S = Integer.parseInt(st.nextToken()); // S<--(T)-->E
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
                 nodes[S].add(new Node(E, T));
                 nodes[E].add(new Node(S, T));
             }
-
-            for (int j = 0; j < W; j++) { //웜홀 정보(단방향)
-                stringTokenizer = new StringTokenizer(reader.readLine());
-                int S = Integer.parseInt(stringTokenizer.nextToken());
-                int E = Integer.parseInt(stringTokenizer.nextToken());
-                int T = Integer.parseInt(stringTokenizer.nextToken()); //감소 시간
-
+            for (int i = 0; i < W; i++) {  // 웜홀
+                st = new StringTokenizer(reader.readLine());
+                int S = Integer.parseInt(st.nextToken()); // S--(T)-->E
+                int E = Integer.parseInt(st.nextToken());
+                int T = Integer.parseInt(st.nextToken());
                 nodes[S].add(new Node(E, -T));
             }
 
-            boolean isNegativeCycle = false;
-
-            for (int j = 1; j <= N; j++) {
-                if (bellmanFord(j)) { //음의 사이클 발생
-                    isNegativeCycle = true;
+            boolean flag = false;
+            for (int i = 1; i <= N; i++) {
+                if (hasNegativeCycle(i, N, nodes)) {
+                    flag = true;
                     break;
                 }
             }
 
-            if (isNegativeCycle) {
-                result.append("YES").append("\n");
-            } else {
-                result.append("NO").append("\n");
-            }
+            System.out.println(flag ? "YES" : "NO");
         }
-
-        System.out.println(result);
     }
 
-    private static boolean bellmanFord(int start) {
-        boolean isUpdated = false;
-
-        Arrays.fill(distance, INF);
+    private static boolean hasNegativeCycle(int start, int N, ArrayList<Node>[] nodes) {
+        int[] distance = new int[N + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
         distance[start] = 0;
 
+        boolean isUpdate = false;
         for (int i = 0; i < N - 1; i++) {
-            isUpdated = false;
-
+            isUpdate = false;
             for (int j = 1; j <= N; j++) {
-                if (distance[j] == INF) {
-                    continue;
-                }
-
-                for (Node node: nodes[j]) {
-                    int newPath = distance[j] + node.weight;  //시작 -> 현재 + 현재 -> 정점
-
-                    if (newPath < distance[node.end]) {
-                        distance[node.end] = newPath;
-                        isUpdated = true;
+                if (distance[j] == Integer.MAX_VALUE) continue;
+                for (Node next: nodes[j]) {
+                    if (distance[j] + next.time < distance[next.val]) {
+                        distance[next.val] = distance[j] + next.time;
+                        isUpdate = true;
                     }
                 }
             }
 
-            if (!isUpdated) {
-                break;
-            }
+            if (!isUpdate) return false;
         }
 
-        if (isUpdated) {
-            for (int j = 1; j <= N; j++) {
-                if (distance[j] == INF) {
-                    continue;
-                }
-
-                for (Node node: nodes[j]) {
-                    int newPath = distance[j] + node.weight;
-
-                    if (newPath < distance[node.end]) {
-                        distance[node.end] = newPath;
-                        return true;
-                    }
-                }
+        for (int j = 1; j <= N; j++) {
+            if (distance[j] == Integer.MAX_VALUE) continue;
+            for (Node next: nodes[j]) {
+                if (distance[j] + next.time < distance[next.val]) return true;
             }
         }
-
         return false;
     }
 }
