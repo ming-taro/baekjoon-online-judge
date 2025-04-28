cf1fe8229c7c7e01ac9ab473ccba0e62a0b6093d
ming-taro <hera002008@naver.com>
2025-04-28 15:31:42 +0900

    [Gold V] Title: 벽장문의 이동, Time: 104 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -11,30 +11,52 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(reader.readLine());       // 벽장 = n개, 문 = (n - 2)개
+        n = Integer.parseInt(reader.readLine());   // 벽장 = n개, 문 = (n - 2)개
         door = new boolean[n + 1];
 
         StringTokenizer st = new StringTokenizer(reader.readLine());
-        int door1 = Integer.parseInt(st.nextToken());  // 문 2개
-        int door2 = Integer.parseInt(st.nextToken());
+        door[Integer.parseInt(st.nextToken())] = true; // 문 2개
+        door[Integer.parseInt(st.nextToken())] = true;
 
-        order = Integer.parseInt(reader.readLine());   // 사용할 벽장 순서
+        order = Integer.parseInt(reader.readLine()); // 사용할 벽장 순서
         target = new int[order];
         for (int i = 0; i < order; i++) {
             target[i] = Integer.parseInt(reader.readLine());
         }
 
-        dfs(door1, door2, 0, 0);
+        dfs(0, 0);
         System.out.println(answer);
     }
 
-    private static void dfs(int door1, int door2, int currentTarget, int moveCount) {
-        if (currentTarget == order) {
+    private static void dfs(int current, int moveCount) {
+        if (current == order) {
             answer = Math.min(answer, moveCount);
             return;
         }
 
-        dfs(target[currentTarget], door2, currentTarget + 1, moveCount + Math.abs(door1 - target[currentTarget])); // 첫 번째 문 사용 -> 첫 번째 문은 현재 타겟으로 변경 + 두 번째 문은 그대로, index 차만큼 이동 횟수 증가
-        dfs(door1, target[currentTarget], currentTarget + 1, moveCount + Math.abs(door2 - target[currentTarget])); // 두 번째 문 사용 -> 두 번째 문은 현재 타겟으로 변경 + 첫 번째 문은 그대로, index 차만큼 이동 횟수 증가
+        if (door[target[current]]) { // 사용 하려는 벽장 앞에 이미 문이 없는 경우
+            dfs(current + 1, moveCount);
+            return;
+        }
+
+        int left = target[current];
+        while (left >= 0 && !door[left]) left--; // 왼쪽 방향으로 벽장 밀기
+        if (left >= 0) {
+            door[target[current]] = true;
+            door[left] = false;
+            dfs(current + 1, moveCount + (target[current] - left));
+            door[target[current]] = false;
+            door[left] = true;
+        }
+
+        int right = target[current];
+        while (right <= n && !door[right]) right++; // 오른쪽 방향으로 벽장 밀기
+        if (right <= n) {
+            door[target[current]] = true;
+            door[right] = false;
+            dfs(current + 1, moveCount + (right - target[current]));
+            door[target[current]] = false;
+            door[right] = true;
+        }
     }
 }
