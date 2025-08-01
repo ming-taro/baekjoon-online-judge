e47b900f65b23678df0cf85d6f324fb31e343991
ming-taro <hera002008@naver.com>
2025-08-02 00:09:46 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        StringBuilder answer = new StringBuilder();
+        dfs(0, new int[M], answer);
+        System.out.println(answer);
+    }
+
+    private static void dfs(int current, int[] numbers, StringBuilder answer) {
+        if (current == M) {
+            for (int num: numbers) {
+                answer.append(num).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        int start = current == 0 ? 1 : numbers[current - 1] + 1;
+        for (int num = start; num <= N; num++) {
+            numbers[current] = num;
+            dfs(current + 1, numbers, answer);
+        }
+    }
+}
