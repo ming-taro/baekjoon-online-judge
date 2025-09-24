f45ace2a78c052c001b23e9085dfd0a966bfd33a
ming-taro <hera002008@naver.com>
2025-09-24 22:44:04 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29152 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -6,38 +6,37 @@ class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(reader.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int[] distance = new int[N];
+        int N = Integer.parseInt(st.nextToken()); // 집의 개수
+        int C = Integer.parseInt(st.nextToken()); // 공유기의 개수
+        int[] house = new int[N]; // 집 좌표
 
         for (int i = 0; i < N; i++) {
-            distance[i] = Integer.parseInt(reader.readLine());
+            house[i] = Integer.parseInt(reader.readLine());
         }
-        Arrays.sort(distance);
+        Arrays.sort(house);
 
-        System.out.println(search(distance, C));
+        System.out.println(search(N, C, house));
     }
 
-    private static int search(int[] distance, int target) {
-        int N = distance.length;
+    private static int search(int N, int C, int[] house) {
         int left = 0;
-        int right = distance[N - 1] - distance[0] + 1;
+        int right = house[N - 1] + 1;
 
         while (left + 1 < right) {
-            int mid = (left + right) / 2;    // 두 공유기 사이의 최소 거리
-            int prev = 0;                    // 첫 번째 집에 무조건 공유기
-            int count = 1;
+            int mid = (left + right) / 2;  // 공유기 설치 간격
+            int count = 1;                 // 공유기 설치 개수 -> 첫번째 집에 설치하고 시작
+            int current = 0;
             for (int i = 1; i < N; i++) {
-                if (distance[i] - distance[prev] >= mid) {
-                    prev = i;
+                if (house[i] - house[current] >= mid) { // 현재 위치에 공유기 설치
+                    current = i;
                     count++;
                 }
             }
-            if (count >= target) { // 공유기를 더 많이 설치할 수 있다 -> 거리를 더 넓혀도 가능성 있음
-                left = mid;
-            } else {               // 공유기가 부족하다 -> 거리를 더 좁혀서 설치해야함
+
+            if (count < C) { // 설치 개수 미달 -> 간격 좁혀서 촘촘히 설치
                 right = mid;
+            } else {
+                left = mid;
             }
         }
 
@@ -45,9 +44,14 @@ class Main {
     }
 }
 /*
-o    o    o
-o    o      o
-1-2--4----8-9
+mid = 공유기 간 최대 거리
+첫번째 집에 무조건 공유기 설치 -> 최대한 멀리 설치하려면 첫집, 마지막 집에 공유기가 설치되게 되어있음
+                           0, 1, 2, 3, 4 집과 공유기 3개를 설치하는 경우
+                           0, 2, 3집에 공유기를 설치한 경우보다 0, 2, 4집에 공유기를 설치해야 거리가 더 넓어짐
+                           따라서 첫번째 집에 공유기가 있다고 가정하고 탐색 진행
 
-1 <= d <= 8
+while -> 이전 공유기 위치와 현재 집 위치 비교 -> mid 거리인 경우 설치
+                                         mid보다 커도 설치 가능하므로 설치
+                                         mid보다 작으면 설치 불가하므로 넘김
+         count가 C를 초과하면 거리를 더 넓혀야 하므로 left = mid / else right = mid
  */
